703aa7fb23686d5f349fd8edcee1fef555291078
Dave Albert <dave.albert@publicisdhealthcare.ie>
2012-09-20 12:17:29 +0100

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -33,6 +33,7 @@ describe "signup" do
       end
     end
 
+
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -41,9 +42,19 @@ describe "signup" do
         fill_in "Confirmation", with: "foobar"
       end
 
+      describe "after saving user" do
+        before { click_button submit }
+        let(:user) { User.find_by_email('user@example.com') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Profile') }
+        it { should have_link('Sign out') }
+      end
+
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
     end
   end
 end
