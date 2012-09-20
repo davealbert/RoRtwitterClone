336c42fba7f695217bcf479e7afae02d961131d7
Dave Albert <dave.albert@publicisdhealthcare.ie>
2012-09-20 16:30:45 +0100

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
 
   def index
@@ -29,6 +30,12 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
+
   def update
     if @user.update_attributes(params[:user])
       flash[:success] = "Profile updated"
@@ -53,6 +60,10 @@ private
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 end
 
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -47,7 +47,7 @@ describe "Authentication" do
     end
   end
 
-describe "authorization" do
+  describe "authorization" do
 
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
@@ -104,5 +104,18 @@ describe "authorization" do
         specify { response.should redirect_to(root_path) }
       end
     end
+
+    describe "as non-admin user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:non_admin) { FactoryGirl.create(:user) }
+
+      before { sign_in non_admin }
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(user) }
+        specify { response.should redirect_to(root_path) }
+      end
+    end
+
   end
 end
